27dca0ce74c74c61e4da79dbee8e778d4c63a8b2
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-10-23 12:22:48 +0800

    feat: initial Capacitor integration

---
+++ android/app/src/main/java/ph/edu/up/dcs/ndsg/opsense/MainActivity.java
@@ -0,0 +1,5 @@
+package ph.edu.up.dcs.ndsg.opsense;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+export default {
+    appId: 'ph.edu.up.dcs.ndsg.opsense',
+    appName: 'Opportunistic Sensing',
+    webDir: 'build',
+    server: { androidScheme: 'https' },
+} satisfies CapacitorConfig;
