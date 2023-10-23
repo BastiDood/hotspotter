0920387214e314f79351365f434f9b4066d8bd1b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-10-23 13:29:09 +0800

    feat: implement geolocation probe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,59 @@
-<p>Hello world!</p>
+<script>
+    import { Geolocation } from '@capacitor/geolocation';
+</script>
+
+{#await Geolocation.requestPermissions({ permissions: ['location'] })}
+    <p>Requesting permissions...</p>
+{:then { location, coarseLocation }}
+    {#if location === 'granted' || coarseLocation === 'granted'}
+        {#await Geolocation.getCurrentPosition({ enableHighAccuracy: true })}
+            <p>Loading...</p>
+        {:then { timestamp, coords: { latitude, longitude, accuracy, altitude, altitudeAccuracy, speed, heading } }}
+            {@const date = new Date(timestamp).toLocaleString()}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Timestamp</th>
+                        <th>Latitude</th>
+                        <th>Longitude</th>
+                        <th>Accuracy (Coordinates)</th>
+                        <th>Altitude</th>
+                        <th>Accuracy (Altitude)</th>
+                        <th>Speed</th>
+                        <th>Heading</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{date}</td>
+                        <td>{latitude}</td>
+                        <td>{longitude}</td>
+                        <td>{accuracy}</td>
+                        <td>{altitude}</td>
+                        <td>{altitudeAccuracy}</td>
+                        <td>{speed}</td>
+                        <td>{heading}</td>
+                    </tr>
+                </tbody>
+            </table>
+        {:catch err}
+            <p>{err}</p>
+        {/await}
+    {:else}
+        <table>
+            <thead>
+                <tr>
+                    <th>Fine Location</th>
+                    <th>Coarse Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{location}</td>
+                    <td>{coarseLocation}</td>
+                </tr>
+            </tbody>
+        </table>
+    {/if}
+{/await}
+
