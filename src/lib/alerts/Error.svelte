c7c9ceceeb5da5bd30a7afe2c96825c2dfd69de3
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-11-05 23:31:51 +0800

    feat: add alert components

---
+++ src/lib/alerts/Error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { ExclamationCircleIcon } from '@krowten/svelte-heroicons';
+</script>
+
+<div class="alert variant-soft-error flex flex-row items-center gap-x-2">
+    <ExclamationCircleIcon class="h-8 w-8" />
+    <div class="!mt-0 contents">
+        <slot />
+    </div>
+</div>
---
+++ src/lib/alerts/Warning.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { ExclamationTriangleIcon } from '@krowten/svelte-heroicons';
+</script>
+
+<div class="alert variant-soft-warning flex flex-row items-center gap-x-2">
+    <ExclamationTriangleIcon class="h-8 w-8" />
+    <div class="!mt-0 contents">
+        <slot />
+    </div>
+</div>
