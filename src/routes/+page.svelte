a82bb7478c84e08c58f1bf4d69a8a03110cb0553
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-11-06 00:09:11 +0800

    feat: prefer alerts for errors/warnings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,15 @@
     import DisplayGeolocation from './DisplayGeolocation.svelte';
     import DisplayLocationPermissions from './DisplayLocationPermissions.svelte';
     import DisplayNetworks from './DisplayNetworks.svelte';
+    import Error from '$lib/alerts/Error.svelte';
     import { Geolocation } from '@capacitor/geolocation';
     import { LightSwitch } from '@skeletonlabs/skeleton';
     import { Network } from '$lib/model.js';
+    import Warning from '$lib/alerts/Warning.svelte';
 </script>
 
 {#await Geolocation.requestPermissions({ permissions: ['location'] })}
-    <p>Requesting permissions...</p>
+    <Warning>Requesting location permissions...</Warning>
 {:then { location, coarseLocation }}
     {#if location === 'granted' || coarseLocation === 'granted'}
         {#await Geolocation.getCurrentPosition({ enableHighAccuracy: true })}
@@ -33,11 +35,11 @@
                     {@const networks = parse(array(Network), payload)}
                     <DisplayNetworks {networks} />
                 {:catch err}
-                    <p>{err}</p>
+                    <Error>{err}</Error>
                 {/await}
             {/await}
         {:catch err}
-            <p>{err}</p>
+            <Error>{err}</Error>
         {/await}
     {:else}
         <DisplayLocationPermissions {location} {coarseLocation} />
