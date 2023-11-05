b46b88a618db9e81803f189e1e1b0ebb2d29b9e8
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-11-06 01:00:04 +0800

    refactor: move WiFi to own page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,5 @@
 <script>
-    import { ProgressBar, ProgressRadial } from '@skeletonlabs/skeleton';
-    import { array, parse } from 'valibot';
-    import DisplayNetworks from './DisplayNetworks.svelte';
     import Error from '$lib/alerts/Error.svelte';
-    import { Network } from '$lib/model.js';
 </script>
 
 <Error>TODO: Leaflet.js Map</Error>
-{#await import('@awesome-cordova-plugins/wifi-wizard-2')}
-    <ProgressRadial />
-{:then { WifiWizard2 }}
-    {#await WifiWizard2.scan()}
-        <ProgressBar />
-    {:then payload}
-        {@const networks = parse(array(Network), payload)}
-        <DisplayNetworks {networks} />
-    {:catch err}
-        <Error>{err}</Error>
-    {/await}
-{/await}
