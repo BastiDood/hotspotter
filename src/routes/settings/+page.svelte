a5ab14686efd7479c930fd6099a3e68ecbd30bb9
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-11-10 03:09:05 +0800

    feat(ui): use default placeholder for URL setting

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+    import { ProgressBar, getToastStore } from '@skeletonlabs/skeleton';
+    import Error from '$lib/alerts/Error.svelte';
     import { Preferences } from '@capacitor/preferences';
     import { assert } from '$lib/assert';
-    import { getToastStore } from '@skeletonlabs/skeleton';
 
     const toast = getToastStore();
 
@@ -18,9 +19,16 @@
     }
 </script>
 
-<form on:submit|self|preventDefault|stopPropagation={({ currentTarget }) => submit(currentTarget)}>
-    <label>
-        <span>URL</span>
-        <input type="url" name="url" required placeholder="URL" class="input px-2 py-1" />
-    </label>
-</form>
+{#await Preferences.get({ key: 'url' })}
+    <ProgressBar />
+{:then { value }}
+    {@const placeholder = value ?? 'https://example.com/'}
+    <form on:submit|self|preventDefault|stopPropagation={({ currentTarget }) => submit(currentTarget)}>
+        <label>
+            <span>URL</span>
+            <input type="url" name="url" required {placeholder} class="input px-2 py-1" />
+        </label>
+    </form>
+{:catch err}
+    <Error>{err}</Error>
+{/await}
