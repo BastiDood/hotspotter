86bb7dd3899ce54c4eb03faeeef5584d310523f0
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-11-13 18:53:09 +0800

    feat(ui): add marker for maps

--- src/routes/map/Map.svelte
+++ src/routes/map/Map.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+    import type { LatLngTuple, Map, Marker } from 'leaflet';
     import { onDestroy, onMount } from 'svelte';
-    import type { Map } from 'leaflet';
     import { assert } from '$lib/assert';
 
     // eslint-disable-next-line init-declarations
@@ -12,13 +12,18 @@
 
     let div = null as HTMLDivElement | null;
     let map = null as Map | null;
-    $: map?.setView([latitude, longitude], zoom);
+    let marker = null as Marker | null;
+
+    $: coords = [latitude, longitude] as LatLngTuple;
+    $: map?.setView(coords, zoom);
+    $: marker?.setLatLng(coords);
 
     onMount(async () => {
         assert(div !== null);
         await import('leaflet/dist/leaflet.css');
         const L = await import('leaflet');
         map = L.map(div);
+        marker = L.marker(coords).addTo(map);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 20,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
