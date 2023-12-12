b2ff5cb730b405c708c00c92c0b2cd81e5c87c0e
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-12-13 01:53:10 +0800

    chore: run linter

--- src/routes/map/[net]/+page.ts
+++ src/routes/map/[net]/+page.ts
@@ -1,8 +1,8 @@
 import { MarkerMode, fetchMarkers } from '$lib/http';
 import type { PageLoad } from './$types';
 import { browser } from '$app/environment';
-import { getUrl } from '$lib/plugins/Config';
 import { error } from '@sveltejs/kit';
+import { getUrl } from '$lib/plugins/Config';
 
 export const load = (async ({ params: { net } }) => {
     if (!browser) return;
