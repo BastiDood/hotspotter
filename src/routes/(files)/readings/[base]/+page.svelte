b056d31e641c5783d7ebc1a7cfb035b76f12a87f
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-10 19:29:27 +0800

    fix(readings): add padding around screen

--- src/routes/(files)/readings/[base]/+page.svelte
+++ src/routes/(files)/readings/[base]/+page.svelte
@@ -19,53 +19,55 @@
     $: ({ reading } = data);
 </script>
 
-{#if reading === null}
-    <Error>Reading not found. Go back to the <a href="/" class="anchor">home page</a>.</Error>
-{:else}
-    {@const {
-        gps,
-        wifi,
-        sim: {
-            strength: { cdma, gsm, lte, nr, tdscdma, wcdma },
-            ...sim
-        },
-    } = reading}
-    <!-- TODO: Render Timestamp and Level -->
-    <div class="space-y-4">
-        <DisplayGeolocation {...gps} />
-        <DisplayNetworks networks={wifi} />
-        <DisplaySim {...sim} />
-        {#if typeof cdma !== 'undefined'}
-            {@const { level, dbm, asu, cdma_dbm, cdma_ecio, cdma_level, evdo_dbm, evdo_ecio, evdo_level, evdo_snr } =
-                cdma}
-            <Common {level} {dbm} {asu} />
-            <DisplayCdma dbm={cdma_dbm} ecio={cdma_ecio} level={cdma_level} />
-            <DisplayEvdo dbm={evdo_dbm} ecio={evdo_ecio} level={evdo_level} snr={evdo_snr} />
-        {/if}
-        {#if typeof gsm !== 'undefined'}
-            {@const { level, dbm, asu, ...payload } = gsm}
-            <Common {level} {dbm} {asu} />
-            <DisplayGsm {...payload} />
-        {/if}
-        {#if typeof lte !== 'undefined'}
-            {@const { level, dbm, asu, ...payload } = lte}
-            <Common {level} {dbm} {asu} />
-            <DisplayLte {...payload} />
-        {/if}
-        {#if typeof nr !== 'undefined'}
-            {@const { level, dbm, asu, ...payload } = nr}
-            <Common {level} {dbm} {asu} />
-            <DisplayNr {...payload} />
-        {/if}
-        {#if typeof tdscdma !== 'undefined'}
-            {@const { level, dbm, asu, ...payload } = tdscdma}
-            <Common {level} {dbm} {asu} />
-            <DisplayTdscdma {...payload} />
-        {/if}
-        {#if typeof wcdma !== 'undefined'}
-            {@const { level, dbm, asu, ...payload } = wcdma}
-            <Common {level} {dbm} {asu} />
-            <DisplayWcdma {...payload} />
-        {/if}
-    </div>
-{/if}
+<div class="p-4">
+    {#if reading === null}
+        <Error>Reading not found. Go back to the <a href="/" class="anchor">home page</a>.</Error>
+    {:else}
+        {@const {
+            gps,
+            wifi,
+            sim: {
+                strength: { cdma, gsm, lte, nr, tdscdma, wcdma },
+                ...sim
+            },
+        } = reading}
+        <!-- TODO: Render Timestamp and Level -->
+        <div class="space-y-4">
+            <DisplayGeolocation {...gps} />
+            <DisplayNetworks networks={wifi} />
+            <DisplaySim {...sim} />
+            {#if typeof cdma !== 'undefined'}
+                {@const { level, dbm, asu, cdma_dbm, cdma_ecio, cdma_level, evdo_dbm, evdo_ecio, evdo_level, evdo_snr } =
+                    cdma}
+                <Common {level} {dbm} {asu} />
+                <DisplayCdma dbm={cdma_dbm} ecio={cdma_ecio} level={cdma_level} />
+                <DisplayEvdo dbm={evdo_dbm} ecio={evdo_ecio} level={evdo_level} snr={evdo_snr} />
+            {/if}
+            {#if typeof gsm !== 'undefined'}
+                {@const { level, dbm, asu, ...payload } = gsm}
+                <Common {level} {dbm} {asu} />
+                <DisplayGsm {...payload} />
+            {/if}
+            {#if typeof lte !== 'undefined'}
+                {@const { level, dbm, asu, ...payload } = lte}
+                <Common {level} {dbm} {asu} />
+                <DisplayLte {...payload} />
+            {/if}
+            {#if typeof nr !== 'undefined'}
+                {@const { level, dbm, asu, ...payload } = nr}
+                <Common {level} {dbm} {asu} />
+                <DisplayNr {...payload} />
+            {/if}
+            {#if typeof tdscdma !== 'undefined'}
+                {@const { level, dbm, asu, ...payload } = tdscdma}
+                <Common {level} {dbm} {asu} />
+                <DisplayTdscdma {...payload} />
+            {/if}
+            {#if typeof wcdma !== 'undefined'}
+                {@const { level, dbm, asu, ...payload } = wcdma}
+                <Common {level} {dbm} {asu} />
+                <DisplayWcdma {...payload} />
+            {/if}
+        </div>
+    {/if}
+</div>
