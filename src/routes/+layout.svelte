22484e5ef0cd1806cb154e7438f1934787ea3d81
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-17 13:27:22 +0800

    refactor(gps): prefer fully qualified icon imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script>
     import './app.css';
     import { AppBar, AppShell, LightSwitch, Toast, initializeStores } from '@skeletonlabs/skeleton';
+    import { ChartBarSquare } from '@steeze-ui/heroicons';
     import { Icon } from '@steeze-ui/svelte-icon';
     import NavBar from './NavBar.svelte';
-    import { ChartBarSquare as src } from '@steeze-ui/heroicons';
     initializeStores();
 </script>
 
 <Toast />
 <AppShell>
     <AppBar slot="header">
-        <Icon {src} slot="lead" class="h-8" />
+        <Icon src={ChartBarSquare} slot="lead" class="h-8" />
         <LightSwitch slot="trail" />
     </AppBar>
     <slot />
