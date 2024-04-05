1ed096904bdf78b70cb91dcb9910cce85a3ab89d
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-05 20:08:33 +0800

    refactor(android): move Wi-Fi helpers to own class

---
+++ android/app/src/main/java/ph/edu/upd/dcs/ndsg/opsense/WifiInfo.java
@@ -0,0 +1,57 @@
+package ph.edu.upd.dcs.ndsg.hotspotter;
+
+import android.os.*;
+import android.net.wifi.*;
+import androidx.annotation.NonNull;
+import com.getcapacitor.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import org.json.JSONArray;
+
+public class WifiInfo {
+    /** Android 11 and below just hard-coded into five tiers. */
+    private static final int DEFAULT_MAX_LEVEL = 5;
+    private @NonNull WifiManager api;
+
+    WifiInfo(@NonNull WifiManager api) {
+        this.api = api;
+    }
+
+    @NonNull
+    private JSObject scanResultToJson(@NonNull ScanResult result) {
+        var now = System.currentTimeMillis();
+        var elapsed = SystemClock.elapsedRealtime();
+        var timestamp = TimeUnit.MICROSECONDS.toMillis(result.timestamp);
+        var unix = now - elapsed + timestamp;
+        var standard = result.getWifiStandard();
+        var json = new JSObject()
+            .put("bssid", result.BSSID)
+            .put("ssid", result.SSID)
+            .put("rssi", result.level)
+            .put("frequency", result.frequency)
+            .put("channel_width", result.channelWidth)
+            .put("center_freq_0", result.centerFreq0)
+            .put("center_freq_1", result.centerFreq1)
+            .put("wifi_timestamp", unix)
+            .put("standard", standard == ScanResult.WIFI_STANDARD_UNKNOWN ? null : standard);
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.R
+            ? json.put("level", api.calculateSignalLevel(result.level, DEFAULT_MAX_LEVEL)).put("max_level", DEFAULT_MAX_LEVEL)
+            : json.put("level", api.calculateSignalLevel(result.level)).put("max_level", api.getMaxSignalLevel());
+    }
+
+    @NonNull
+    public JSObject getScanResults() {
+        var results = api.getScanResults();
+        results.sort((a, b) -> b.level - a.level);
+        var list = results
+            .stream()
+            .map(this::scanResultToJson)
+            .collect(Collectors.toList());
+        return new JSObject().put("results", new JSONArray(list));
+    }
+
+    @NonNull
+    public JSObject startScan() {
+        return new JSObject().put("ok", api.startScan());
+    }
+}
--- android/app/src/main/java/ph/edu/upd/dcs/ndsg/opsense/WifiInfoPlugin.java
+++ android/app/src/main/java/ph/edu/upd/dcs/ndsg/opsense/WifiInfoPlugin.java
@@ -1,15 +1,12 @@
 package ph.edu.upd.dcs.ndsg.hotspotter;
 
 import android.Manifest;
-import android.content.*;
-import android.net.wifi.*;
-import android.os.*;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.*;
 import com.getcapacitor.annotation.*;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.*;
-import org.json.JSONArray;
+import java.util.HashSet;
 
 @CapacitorPlugin(
     name = "WifiInfo",
@@ -26,56 +23,22 @@ public class WifiInfoPlugin extends Plugin {
     WifiManager.ScanResultsCallback callback = new WifiManager.ScanResultsCallback() {
         @Override
         public void onScanResultsAvailable() {
-            var json = scanResultsToResultJson(getApi());
+            var api = ContextCompat.getSystemService(getActivity(), WifiManager.class);
+            var json = new WifiInfo(api).getScanResults();
             for (var id : watchers) getBridge().getSavedCall(id).resolve(json);
         }
     };
 
-    /** Android 11 and below just hard-coded into five tiers. */
-    private static final int DEFAULT_MAX_LEVEL = 5;
-
-    private JSObject scanResultToJson(WifiManager api, ScanResult result) {
-        var now = System.currentTimeMillis();
-        var elapsed = SystemClock.elapsedRealtime();
-        var timestamp = TimeUnit.MICROSECONDS.toMillis(result.timestamp);
-        var unix = now - elapsed + timestamp;
-
-        var standard = result.getWifiStandard();
-        var json = new JSObject()
-            .put("bssid", result.BSSID)
-            .put("ssid", result.SSID)
-            .put("rssi", result.level)
-            .put("frequency", result.frequency)
-            .put("channel_width", result.channelWidth)
-            .put("center_freq_0", result.centerFreq0)
-            .put("center_freq_1", result.centerFreq1)
-            .put("wifi_timestamp", unix)
-            .put("standard", standard == ScanResult.WIFI_STANDARD_UNKNOWN ? null : standard);
-
-        return Build.VERSION.SDK_INT < Build.VERSION_CODES.R
-            ? json.put("level", api.calculateSignalLevel(result.level, DEFAULT_MAX_LEVEL)).put("max_level", DEFAULT_MAX_LEVEL)
-            : json.put("level", api.calculateSignalLevel(result.level)).put("max_level", api.getMaxSignalLevel());
-    }
-
-    private JSObject scanResultsToResultJson(WifiManager api) {
-        var results = api.getScanResults();
-        results.sort((a, b) -> b.level - a.level);
-        var list = results.stream().map(result -> scanResultToJson(api, result)).collect(Collectors.toList());
-        return new JSObject().put("results", new JSONArray(list));
-    }
-
-    private WifiManager getApi() {
-        return (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
-    }
-
     @PluginMethod()
     public void startScan(PluginCall ctx) {
-        ctx.resolve(new JSObject().put("ok", getApi().startScan()));
+        var api = ContextCompat.getSystemService(getActivity(), WifiManager.class);
+        ctx.resolve(new WifiInfo(api).startScan());
     }
 
     @PluginMethod()
     public void getScanResults(PluginCall ctx) {
-        ctx.resolve(scanResultsToResultJson(getApi()));
+        var api = ContextCompat.getSystemService(getActivity(), WifiManager.class);
+        ctx.resolve(new WifiInfo(api).getScanResults());
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
@@ -91,8 +54,9 @@ public class WifiInfoPlugin extends Plugin {
 
         if (shouldInit) {
             var act = getActivity();
-            var api = (WifiManager) act.getSystemService(Context.WIFI_SERVICE);
-            api.registerScanResultsCallback(act.getMainExecutor(), callback);
+            var api = ContextCompat.getSystemService(act, WifiManager.class);
+            var exe = ContextCompat.getMainExecutor(act);
+            api.registerScanResultsCallback(exe, callback);
         }
     }
 
@@ -107,7 +71,12 @@ public class WifiInfoPlugin extends Plugin {
         watchers.remove(id);
         getBridge().releaseCall(id);
 
-        if (watchers.isEmpty()) getApi().unregisterScanResultsCallback(callback);
+        if (watchers.isEmpty()) {
+            var act = getActivity();
+            var api = ContextCompat.getSystemService(act, WifiManager.class);
+            api.unregisterScanResultsCallback(callback);
+        }
+
         ctx.resolve();
     }
 }
