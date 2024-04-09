4522b244e28ce8b0059f28ed256be9b5a84c63a2
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-09 21:46:43 +0800

    chore: rename `/meta` to `/meta.json`

--- src/routes/info/+page.server.js
+++ src/routes/info/+page.server.js
@@ -2,7 +2,7 @@ import { Meta } from '$lib/models/meta';
 import { parse } from 'valibot';
 
 export async function load({ fetch }) {
-    const response = await fetch('/meta');
+    const response = await fetch('/meta.json');
     const json = await response.json();
     return parse(Meta, json, { abortEarly: true });
 }
