0e9b8e858085f8094989e91694c35e451331c35a
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-23 13:56:27 +0800

    feat(ui): use new logo for top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
 <script>
     import './app.css';
     import { AppBar, AppShell, LightSwitch, Toast, initializeStores } from '@skeletonlabs/skeleton';
-    import { ChartBarSquare } from '@steeze-ui/heroicons';
-    import { Icon } from '@steeze-ui/svelte-icon';
     import NavBar from './NavBar.svelte';
+    import logo from '$lib/logo/hotspotter.svg?raw';
     initializeStores();
 </script>
 
 <Toast />
 <AppShell>
     <AppBar slot="header">
-        <Icon src={ChartBarSquare} slot="lead" class="h-8" />
+        <div slot="lead" class="fill-token size-6">
+            <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+            {@html logo}
+        </div>
         <LightSwitch slot="trail" />
     </AppBar>
     <slot />
