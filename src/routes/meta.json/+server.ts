edbb7894838e77a18b13848c6975f7096110a0c1
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-28 03:51:39 +0800

    feat(api): reintroduce metadata endpoint

---
+++ src/routes/meta.json/+server.ts
@@ -0,0 +1,12 @@
+import type { Meta } from '$lib/models/meta';
+import { json } from '@sveltejs/kit';
+import { spawn } from 'child_process';
+import { text } from 'stream/consumers';
+
+export const prerender = true;
+
+export async function GET() {
+    const timestamp = new Date().toISOString();
+    const raw = await text(spawn('git', ['rev-parse', 'HEAD']).stdout);
+    return json({ timestamp, revision: raw.trim() } satisfies Meta);
+}
