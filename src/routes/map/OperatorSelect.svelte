f8dd6beb9d3b8fb4e1edde0408661fe5bd3eaaa4
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-28 22:22:47 +0800

    refactor(map): reorder operator select

--- src/routes/map/OperatorSelect.svelte
+++ src/routes/map/OperatorSelect.svelte
@@ -7,11 +7,11 @@
 </script>
 
 <select {name} bind:value class="select variant-form-material pointer-events-auto" transition:slide={{ duration: 300 }}>
+    <option value={null}>All Operators</option>
+    <option value={515}>All Philippine Operators</option>
     <option value={51502}>Globe Telecom</option>
     <option value={51503}>Smart Communications</option>
     <option value={51505}>Sun Cellular</option>
     <option value={51524}>ABS-CBN Mobile</option>
     <option value={51566}>DITO Telecommunity</option>
-    <option value={515}>All Philippine Operators</option>
-    <option value={null}>All Operators</option>
 </select>
