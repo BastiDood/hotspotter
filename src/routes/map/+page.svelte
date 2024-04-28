495de4878554e68be8785791026b68d9b2cd5dce
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-28 16:07:07 +0800

    fix(map): remove non-existent prop

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -4,10 +4,6 @@
     import { Geolocation } from '@capacitor/geolocation';
     import Map from './Map.svelte';
 
-    // eslint-disable-next-line init-declarations
-    export let data;
-    $: ({ carriers } = data);
-
     /** @param {number} code */
     function messageFromGpsErrorCode(code) {
         switch (code) {
