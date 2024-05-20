73e0a922fb7c562fc23b3d00426538c392eb727b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-05-20 12:34:01 +0800

    fix(home): refresh list of readings per page visit

--- src/routes/(files)/+layout.js
+++
@@ -1,8 +0,0 @@
-import { browser, building } from '$app/environment';
-import { Capacitor } from '@capacitor/core';
-import { read } from '$lib/plugins/Cache';
-
-export async function load() {
-    const readings = !building && browser && Capacitor.isNativePlatform() ? Array.from(await read()) : [];
-    return { readings };
-}
--- src/routes/(files)/+page.js
+++ src/routes/(files)/+page.js
@@ -1,8 +1,13 @@
 import { browser, building } from '$app/environment';
 import { Capacitor } from '@capacitor/core';
 import { checkPermissions } from '$lib/plugins/Loop';
+import { read } from '$lib/plugins/Cache';
+
+async function getData() {
+    const [generator, permissions] = await Promise.all([read(), checkPermissions()]);
+    return { readings: Array.from(generator), permissions };
+}
 
 export async function load() {
-    const permissions = !building && browser && Capacitor.isNativePlatform() ? await checkPermissions() : {};
-    return { permissions };
+    return !building && browser && Capacitor.isNativePlatform() ? await getData() : { readings: [], permissions: {} };
 }
