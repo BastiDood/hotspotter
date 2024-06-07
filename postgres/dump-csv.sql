575e8397bab6cd2fd0f0090ca142285d89a7f49f
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-08 03:06:30 +0800

    feat(db): add script for exporting CSV

---
+++ postgres/dump-csv.sql
@@ -0,0 +1,8 @@
+\copy hotspotter.cdma TO 'cdma.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.gsm TO 'gsm.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.lte TO 'lte.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.nr TO 'nr.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.tdscdma TO 'tdscdma.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.wcdma TO 'wcdma.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.readings TO 'readings.csv' WITH (FORMAT CSV, HEADER);
+\copy hotspotter.wifi TO 'wifi.csv' WITH (FORMAT CSV, HEADER);
