8129eeff0562b8eb13bd7cca1dd1b36e698b913e
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-07 16:21:03 +0800

    refactor: revert PostCSS configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
+
 export default defineConfig({
     plugins: [sveltekit(), purgeCss()],
     build: { assetsInlineLimit: 0 },
+    css: { postcss: { plugins: [tailwind, autoprefixer] } },
 });
